8960cffe4b2bf37ce8c726c71db22bbe1457bee9
haydenledean <52312596+haydenledean@users.noreply.github.com>
2020-02-12 11:15:57 +0100

    The 1.0.0 is out !

---
+++ FileEx.bat
@@ -0,0 +1,70 @@
+@echo off
+title FileEx
+color 1f
+goto main
+
+:main
+goto title
+goto script1
+
+:title
+echo ====== FileEx ======
+echo v1.0.0
+echo By haydenledean
+echo ====================
+echo Press any key to continue...
+echo ====================
+pause >nul
+
+:script1
+echo fileviewer - See a file !
+echo ====================
+set fileex="__"
+set /p fileex="FileEx> "
+goto command
+
+:command
+if %fileex%==fileviewer    (goto script2)
+goto script1
+
+:script2
+cls
+echo ===================
+echo === File Viewer ===
+echo ===================
+set file="__"
+set /p file="File ?> "
+goto script3
+
+:script3
+cls
+if not exist %file% (goto filerror)
+if exist %file%     (goto filefound)
+
+:filerror
+echo %cd% don't exist
+echo Press any key to continue...
+pause >nul
+cls
+goto main
+
+:filefound
+echo %file% exist
+echo Content :
+set mycommand="__"
+set /p mycommand="Type the file ? Y/N> "
+if %mycommand%==Y    (goto typefile)
+if %mycommand%==y    (goto typefile)
+if %mycommand%==N    (exit)
+if %mycommand%==n    (exit)
+if %mycommand%==     (exit)
+pause >nul
+
+:typefile
+cls
+type %file%
+echo ====================
+echo Press any key to continue...
+pause >nul
+cls
+goto main
